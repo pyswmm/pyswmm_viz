5f4f1eb195d179419bd2291428ae73310d56317a
xiaofz <xiaofz@gmail.com>
2023-11-14 00:43:52 -0500

    update

---
+++ test.py
@@ -0,0 +1,3 @@
+print('Hello world')
+
+import streamlit as st
--- viz.py
+++ viz.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
-from pyswmm import Simulation, Nodes, Links
-from swmm.toolkit.shared_enum import SubcatchAttribute, NodeAttribute, LinkAttribute
-from pyswmm import Output
+#from pyswmm import Simulation, Nodes, Links
+#from swmm.toolkit.shared_enum import SubcatchAttribute, NodeAttribute, LinkAttribute
+#from pyswmm import Output
 import numpy as np
 from swmm_api.input_file import read_inp_file, SwmmInput, section_labels as sections
 from swmm_api import swmm5_run, read_out_file
@@ -41,7 +41,7 @@ if uploaded_file is not None:
 
     #print(inp)
 else:
-    uploaded_file = "inp/Example1.inp"
+    uploaded_file = "pyswmm_viz/inp/Example1.inp"
     inp = SwmmInput.read_file(uploaded_file)
   
 
@@ -113,10 +113,29 @@ def twoD_view(inp):
     # add xy coordinates to outfalls
     outfalls_coord = pd.concat([outfalls, coordinates], axis=1, join="inner")
     
-    # add xy coordinates to conduits
+    
+        # add xy coordinates to conduits
+    junctions_coord['node_id'] = junctions_coord.index
+    outfalls_coord['node_id'] = outfalls_coord.index
+    all_nodes = pd.concat([junctions_coord, outfalls_coord], ignore_index=True)#need to add more nodes like storage, ect.
+    conduits['conduit_id']=conduits.index
+
+    all_nodes.set_index('node_id', inplace=True)
+    conduits.set_index('from_node', inplace=True)
+    
+    # Merging the 'x' and 'y' columns from the first DataFrame into the second DataFrame based on the index
+    conduits = conduits.join(all_nodes[['x', 'y']])
+    #rename xy columns 
+    conduits.rename(columns={'x': 'from_x', 'y': 'from_y'}, inplace=True)
+    conduits.set_index('to_node', inplace=True)
+    conduits = conduits.join(all_nodes[['x', 'y']])
+    conduits.rename(columns={'x': 'to_x', 'y': 'to_y'}, inplace=True)
+    
+
+    # st.dataframe(junctions_coord) 
+    # st.dataframe(all_nodes)
+    #add dropdown menu for selecting dataframe
     st.dataframe(conduits)
-
-
     
     # Create scatter plot using go.Scatter
     fig = go.Figure()
@@ -135,17 +154,26 @@ def twoD_view(inp):
     )
     
     # add polygon trace for subcatchments
+    num = 0
     for polygon in subs_coord['polygon']:
         x,y = zip(*polygon)
         fig.add_trace(go.Scatter(
                         x=list(x) + [x[0]],  # Close the polygon by repeating the first point
                         y=list(y) + [y[0]],
                         fill="toself",
-                        fillcolor='rgba(0,176,246,0.2)',
-                        line_color='rgba(0,176,246,0.8)',
+                        fillcolor='rgba(92,96,232,0.2)',
+                        line_color='rgba(92,96,232,0.2)',
                     )
                     )
-    # add symbol trace for raingages
+        
+        fig.add_trace(go.Scatter(x = [sum(x)/len(x)],
+                                 y = [sum(y)/len(y)],
+                                 mode = 'text',
+                                 text = 'Sub ' + subs_coord.index[num],
+                                 textfont = dict(color='black', size=12),))
+        num = num + 1                        
+        
+    # # add symbol trace for raingages
     fig.add_trace(
         go.Scatter(
             x=raingages_coord['x'],
@@ -170,6 +198,24 @@ def twoD_view(inp):
         )
     )   
     
+
+    # add trace for conduits
+    for conduit in conduits.itertuples():
+
+        fig.add_trace(go.Scatter(x = [conduit[8], conduit[10]],
+                                 y = [conduit[9], conduit[11]],
+                                 mode = 'lines',
+                                 line = dict(width = 3, color = 'crimson'),
+                                 )
+                      )
+        fig.add_trace(go.Scatter(x = [(conduit[8]+conduit[10])/2],
+                                 y = [(conduit[9]+conduit[11])/2],
+                                 mode = 'text',
+                                 showlegend=True,
+                                 text = 'Conduit '+ conduit[7],
+                                 textfont = dict(color='black', size=12),))
+   
+    
     # Set the x and y axes to have the same range
     x_min = junctions_coord['x'].min()
     x_max = junctions_coord['x'].max()
@@ -181,10 +227,166 @@ def twoD_view(inp):
         yaxis=dict(scaleratio=1, showgrid=False)
     )
     #fig.update_layout(yaxis_scaleanchor="x")
-    fig.update_layout(
+    fig.update_layout(showlegend=False,
                     autosize=False,
-                    width=600,
-                    height=600,)
+                    width=800,
+                    height=800,)
+    # Customize layout
+    fig.update_xaxes(title_text='X-axis')
+    fig.update_yaxes(title_text='Y-axis')
+    fig.update_layout(title='Node Plot')
+    
+    
+    # Display the Plotly figure in Streamlit
+    st.plotly_chart(fig)
+    
+    return None
+
+# 3D view page
+def threeD_view(inp):
+    raingages = inp[sections.RAINGAGES].frame
+    junctions = inp[sections.JUNCTIONS].frame
+    subs = inp[sections.SUBCATCHMENTS].frame
+    outfalls = inp[sections.OUTFALLS].frame
+    conduits = inp[sections.CONDUITS].frame
+    
+    coordinates = inp[sections.COORDINATES].frame    #junctions
+    polygons = inp[sections.POLYGONS].frame  #subcatchment
+    symbols = inp[sections.SYMBOLS].frame  #raingage
+    
+    #frames = [df1, df2, df3]
+    #result = pd.concat(frames)
+    
+    # add xy coordinates to junctions
+    junctions_coord = pd.concat([junctions, coordinates], axis=1, join="inner")
+
+    # add xy coordinates to subcatchments
+    subs_coord = pd.concat([subs, polygons], axis=1, join="inner")
+
+    # add xy coordinates to raingages
+    raingages_coord = pd.concat([raingages, symbols], axis=1, join="inner")
+    
+    # add xy coordinates to outfalls
+    outfalls_coord = pd.concat([outfalls, coordinates], axis=1, join="inner")
+    
+    
+        # add xy coordinates to conduits
+    junctions_coord['node_id'] = junctions_coord.index
+    outfalls_coord['node_id'] = outfalls_coord.index
+    all_nodes = pd.concat([junctions_coord, outfalls_coord], ignore_index=True)#need to add more nodes like storage, ect.
+    conduits['conduit_id']=conduits.index
+
+    all_nodes.set_index('node_id', inplace=True)
+    conduits.set_index('from_node', inplace=True)
+    
+    # Merging the 'x' and 'y' columns from the first DataFrame into the second DataFrame based on the index
+    conduits = conduits.join(all_nodes[['x', 'y']])
+    #rename xy columns 
+    conduits.rename(columns={'x': 'from_x', 'y': 'from_y'}, inplace=True)
+    conduits.set_index('to_node', inplace=True)
+    conduits = conduits.join(all_nodes[['x', 'y']])
+    conduits.rename(columns={'x': 'to_x', 'y': 'to_y'}, inplace=True)
+    
+
+    # st.dataframe(junctions_coord) 
+    # st.dataframe(all_nodes)
+    #add dropdown menu for selecting dataframe
+    st.dataframe(conduits)
+    
+    # Create scatter plot using go.Scatter
+    fig = go.Figure()
+
+    # Add scatter trace
+    fig.add_trace(
+        go.Scatter(
+            x=junctions_coord['x'],
+            y=junctions_coord['y'],
+            mode='markers+text',
+            text=junctions_coord.index,
+            textposition='top center',
+            marker=dict(size=12, opacity=0.8),
+            
+        )
+    )
+    
+    # add polygon trace for subcatchments
+    num = 0
+    for polygon in subs_coord['polygon']:
+        x,y = zip(*polygon)
+        fig.add_trace(go.Scatter(
+                        x=list(x) + [x[0]],  # Close the polygon by repeating the first point
+                        y=list(y) + [y[0]],
+                        fill="toself",
+                        fillcolor='rgba(92,96,232,0.2)',
+                        line_color='rgba(92,96,232,0.2)',
+                    )
+                    )
+        
+        fig.add_trace(go.Scatter(x = [sum(x)/len(x)],
+                                 y = [sum(y)/len(y)],
+                                 mode = 'text',
+                                 text = 'Sub ' + subs_coord.index[num],
+                                 textfont = dict(color='black', size=12),))
+        num = num + 1                        
+        
+    # # add symbol trace for raingages
+    fig.add_trace(
+        go.Scatter(
+            x=raingages_coord['x'],
+            y=raingages_coord['y'],
+            mode='markers+text',
+            text=raingages_coord.index,
+            textposition='top center',
+            marker=dict(size=12, opacity=0.8),
+            
+        )
+    )
+    
+     # add symbol trace for outfalls
+    fig.add_trace(
+        go.Scatter(
+            x=outfalls_coord['x'],
+            y=outfalls_coord['y'],
+            mode='markers+text',
+            text=outfalls_coord.index,
+            textposition='top center',
+            marker=dict(size=12, opacity=0.8),  
+        )
+    )   
+    
+
+    # add trace for conduits
+    for conduit in conduits.itertuples():
+
+        fig.add_trace(go.Scatter(x = [conduit[8], conduit[10]],
+                                 y = [conduit[9], conduit[11]],
+                                 mode = 'lines',
+                                 line = dict(width = 3, color = 'crimson'),
+                                 )
+                      )
+        fig.add_trace(go.Scatter(x = [(conduit[8]+conduit[10])/2],
+                                 y = [(conduit[9]+conduit[11])/2],
+                                 mode = 'text',
+                                 showlegend=True,
+                                 text = 'Conduit '+ conduit[7],
+                                 textfont = dict(color='black', size=12),))
+   
+    
+    # Set the x and y axes to have the same range
+    x_min = junctions_coord['x'].min()
+    x_max = junctions_coord['x'].max()
+    y_min = junctions_coord['y'].min()
+    y_max = junctions_coord['y'].max()
+    
+    fig.update_layout(
+        xaxis=dict(scaleratio=1, showgrid=False),
+        yaxis=dict(scaleratio=1, showgrid=False)
+    )
+    #fig.update_layout(yaxis_scaleanchor="x")
+    fig.update_layout(showlegend=False,
+                    autosize=False,
+                    width=800,
+                    height=800,)
     # Customize layout
     fig.update_xaxes(title_text='X-axis')
     fig.update_yaxes(title_text='Y-axis')
@@ -204,4 +406,5 @@ elif options == 'Stats':
     stats(inp)
 elif options == '2D view':
     twoD_view(inp)
-
+elif options == '3D view':
+    threeD_view(inp)
